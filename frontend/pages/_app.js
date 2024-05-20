b42bfb65f68662cc39eb938e351a693e2b4af3df
GreatnessGrace <graceunstoppable7@gmail.com>
2024-05-20 17:46:43 +0530

    query client in app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+import '../styles/globals.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
