f9c6dca42c4891119724a417ae85d58749bbac76
GreatnessGrace <graceunstoppable7@gmail.com>
2024-05-21 14:07:53 +0530

    extra model will be refactor later on

---
+++ backend/models/projects.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const Project  = (sequelize, DataTypes) => {
+  class Project extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Project.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    pdfUrl: DataTypes.STRING,
+    status: DataTypes.STRING,
+    embedding: DataTypes.JSON
+  }, {
+    sequelize,
+    modelName: 'Project',
+  });
+  return Project;
+};
+module.exports = Project;
+
+
