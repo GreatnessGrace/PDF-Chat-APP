97763112acb0e7102694f2de2d4e1728ef334270
GreatnessGrace <graceunstoppable7@gmail.com>
2024-05-22 11:02:42 +0530

    color update

--- frontend/pages/projects/[id].js
+++ frontend/pages/projects/[id].js
@@ -21,17 +21,17 @@ export default function Project() {
         setAnswer(data.answer);
     };
 
-    if (isLoading) return <div className="flex items-center justify-center h-screen bg-gray-100">Loading...</div>;
-    if (error) return <div className="flex items-center justify-center h-screen bg-gray-100">Error loading project</div>;
+    if (isLoading) return <div className="flex items-center justify-center h-screen bg-gray-200">Loading...</div>;
+    if (error) return <div className="flex items-center justify-center h-screen bg-gray-200">Error loading project</div>;
 
     return (
-        <div className='flex items-center justify-center min-h-screen bg-gray-100 p-4'>
-            <div className="container mx-auto w-full max-w-4xl p-6 bg-white shadow-lg rounded-lg">
-                <h1 className="text-3xl font-bold mb-6 text-center text-blue-600">Ask me Anything?</h1>
+        <div className='flex items-center justify-center min-h-screen bg-gray-200 p-4'>
+            <div className="container mx-auto w-full max-w-4xl p-6 bg-white shadow-md rounded-lg">
+                <h1 className="text-3xl font-bold mb-6 text-center text-indigo-600">Ask me Anything</h1>
                 <div className="container mx-auto p-4">
                     <h1 className="text-2xl font-bold text-center mb-4">Project Title: {data?.title}</h1>
                     <p className='text-center mb-4'>Project Description: {data?.description}</p>
-                    <p className={`text-center mb-4 font-semibold ${data?.status === 'created' ? 'text-green-500' : 'text-red-500'}`}>Status: {data?.status}</p>
+                    <p className={`text-center mb-4 font-semibold ${data?.status === 'created' ? 'text-green-600' : 'text-red-600'}`}>Status: {data?.status}</p>
 
                     {data?.status === 'created' && (
                         <div className='mb-4 flex items-center justify-center'>
@@ -43,7 +43,7 @@ export default function Project() {
                                 onChange={(e) => setQuestion(e.target.value)}
                             />
                             <button
-                                className='ml-2 mt-1 p-2 py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700'
+                                className='ml-2 mt-1 p-2 py-2 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700'
                                 onClick={handleAsk}
                             >
                                 Ask
